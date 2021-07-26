040e4dbace666f9bf99dfa766e71933591315cd5
dhrumil29796 <shahdhrumil1060@gmail.com>
2021-07-26 10:11:15 +0530

    Formatted the Utils folder files

--- src/main/java/com/dal/database/utils/Application.java
+++ src/main/java/com/dal/database/utils/Application.java
@@ -7,10 +7,10 @@ import java.util.Scanner;
 
 public class Application {
 
-    public static String pathOfUsers = "./RawData/LoginUsersData/AllUsers";
-    public static String pathOfDataBase = "./RawData/databases/allDatabases";
+  public static String pathOfUsers = "./RawData/LoginUsersData/AllUsers";
+  public static String pathOfDataBase = "./RawData/databases/allDatabases";
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         /*RegisterUser registerUser = new RegisterUser();
         registerUser.registerNewUser("root", "root123");
         registerUser.registerNewUser("manu", "manu123");
@@ -19,7 +19,7 @@ public class Application {
 //        AttemptLogin login = new AttemptLogin();
 //        login.loginUser(new Scanner(System.in));
 //
-        //SplitQuery splitQuery = new SplitQuery(" Select (name, value, data) from oss_ref_data where name = 'erfverf vrfvr';");
+    //SplitQuery splitQuery = new SplitQuery(" Select (name, value, data) from oss_ref_data where name = 'erfverf vrfvr';");
 //        SplitQuery splitQuery = new SplitQuery(" CREATE TABLE Persons (" +
 //                "    PersonID int," +
 //                "    LastName String," +
@@ -27,11 +27,11 @@ public class Application {
 //                "    Address String," +
 //                "    City String" +
 //                ");");
-        //splitQuery.splitQueryTokens();
+    //splitQuery.splitQueryTokens();
 
-        InputFromUser.getInstance().inputsFromUser(new Scanner(System.in));
+    InputFromUser.getInstance().inputsFromUser(new Scanner(System.in));
 
 
-    }
+  }
 
 }
--- src/main/java/com/dal/database/utils/InputFromUser.java
+++ src/main/java/com/dal/database/utils/InputFromUser.java
@@ -13,325 +13,300 @@ import java.util.regex.Pattern;
 
 public class InputFromUser {
 
-    private InputFromUser(){
-        PrintInfo.getInstance().printMessage("\n\t####################################\n");
-        PrintInfo.getInstance().printMessage("Welcome to DVM Distributed Database:\n");
-        PrintInfo.getInstance().printMessage("\n\t####################################\n");
+  private static InputFromUser instance = null;
 
-        try {
-            new BasicFolderStructure();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
+  private InputFromUser() {
+    PrintInfo.getInstance().printMessage("\n\t####################################\n");
+    PrintInfo.getInstance().printMessage("Welcome to DVM Distributed Database:\n");
+    PrintInfo.getInstance().printMessage("\n\t####################################\n");
+    try {
+      new BasicFolderStructure();
+    } catch (IOException ioException) {
+      ioException.printStackTrace();
     }
+  }
 
-    private static InputFromUser instance = null;
-
-    public static InputFromUser getInstance(){
-        if(instance == null){
-            instance = new InputFromUser();
-        }
-        return instance;
+  public static InputFromUser getInstance() {
+    if (instance == null) {
+      instance = new InputFromUser();
     }
+    return instance;
+  }
 
-    public void inputsFromUser(Scanner sc){
-        FetchDataFromFiles.fetchAllDatabases();
-        FetchDataFromFiles.fetchAllUsers();
-        AttemptLogin attemptLogin = new AttemptLogin();
-        if(!attemptLogin.loginUser(new Scanner(System.in))){
-            System.exit(0);
-        }
-        for(;;) {
-
-            System.out.print(BasicInformation.getInstance().getLoginUser() + "@DVM.sql >>>>> ");
-            String input = sc.nextLine();
-            SplitQuery splitQuery = new SplitQuery(input);
-            List<String> inputTokens = splitQuery.splitQueryTokens();
-            if(!evaluateInput(inputTokens)){
-                continue;
-            }
-
-        }
+  public void inputsFromUser(Scanner sc) {
+    FetchDataFromFiles.fetchAllDatabases();
+    FetchDataFromFiles.fetchAllUsers();
+    AttemptLogin attemptLogin = new AttemptLogin();
+    if (!attemptLogin.loginUser(new Scanner(System.in))) {
+      System.exit(0);
     }
-
-    private boolean evaluateInput(List<String> tokens){
-        if(tokens == null || tokens.size()<=0){
-            return false;
-        }
-
-        List<String> newTokens = getSubTokens(tokens);
-
-        switch (tokens.get(0).toLowerCase()){
-            case "exit":{
-                return(exitQuery(newTokens));
-            }
-            case "commit":{
-                return(commit(newTokens));
-
-            }
-            case "create":{
-                return(create(newTokens));
-            }
-            case "use":{
-                return(useDatabase(newTokens));
-            }
-            case "show":{
-                return showDatabases(newTokens);
-            }
-
-            default :{
-                PrintInfo.getInstance().commandError();
-                return false;
-            }
-        }
-
+    for (; ; ) {
+      System.out.print(BasicInformation.getInstance().getLoginUser() + "@DVM.sql >>>>> ");
+      String input = sc.nextLine();
+      SplitQuery splitQuery = new SplitQuery(input);
+      List<String> inputTokens = splitQuery.splitQueryTokens();
+      if (!evaluateInput(inputTokens)) {
+        continue;
+      }
     }
+  }
 
-    private boolean useDatabase(List<String> tokens){
-        if(!tokenListValidation(tokens)){
-            PrintInfo.getInstance().commandError();
-            return false;
-        }
-        String databaseName = tokens.get(0);
-        tokens = getSubTokens(tokens);
-
-        if(!(tokens == null || tokens.size()<=0 || (";").equals(tokens.get(0)))){
-            PrintInfo.getInstance().commandError();
-            return false;
-        }
-        UseDatabase useDatabase = new UseDatabase();
-        return useDatabase.UseThisDatabase(databaseName);
-
+  private boolean evaluateInput(List<String> tokens) {
+    if (tokens == null || tokens.size() <= 0) {
+      return false;
     }
-
-    private boolean showDatabases(List<String> tokens){
-        if(!tokenListValidation(tokens)){
-            PrintInfo.getInstance().commandError();
-            return false;
-        }
-        String query = tokens.get(0);
-        tokens = getSubTokens(tokens);
-
-        if(!endOfQuery(tokens) || !"databases".equalsIgnoreCase(query)){
-            PrintInfo.getInstance().commandError();
-            return false;
-        }
-        ShowDatabases showDatabases = new ShowDatabases();
-        showDatabases.showAllDatabases();
-
-        return true;
-
-    }
-
-    private boolean exitQuery(List<String> tokens){
-        if(tokens == null || tokens.size()<=0 || (";").equals(tokens.get(0))){
-            PrintInfo.getInstance().printMessage("\n\tExit\n\t");
-            System.exit(0);
-        }
-
+    List<String> newTokens = getSubTokens(tokens);
+    switch (tokens.get(0).toLowerCase()) {
+      case "exit" -> {
+        return (exitQuery(newTokens));
+      }
+      case "commit" -> {
+        return (commit(newTokens));
+      }
+      case "create" -> {
+        return (create(newTokens));
+      }
+      case "use" -> {
+        return (useDatabase(newTokens));
+      }
+      case "show" -> {
+        return showDatabases(newTokens);
+      }
+      default -> {
         PrintInfo.getInstance().commandError();
         return false;
+      }
+    }
+  }
+
+  private boolean useDatabase(List<String> tokens) {
+    if (!tokenListValidation(tokens)) {
+      PrintInfo.getInstance().commandError();
+      return false;
+    }
+    String databaseName = tokens.get(0);
+    tokens = getSubTokens(tokens);
+    if (!(tokens == null || tokens.size() <= 0 || (";").equals(tokens.get(0)))) {
+      PrintInfo.getInstance().commandError();
+      return false;
+    }
+    UseDatabase useDatabase = new UseDatabase();
+    return useDatabase.UseThisDatabase(databaseName);
+  }
+
+  private boolean showDatabases(List<String> tokens) {
+    if (!tokenListValidation(tokens)) {
+      PrintInfo.getInstance().commandError();
+      return false;
+    }
+    String query = tokens.get(0);
+    tokens = getSubTokens(tokens);
+
+    if (!endOfQuery(tokens) || !"databases".equalsIgnoreCase(query)) {
+      PrintInfo.getInstance().commandError();
+      return false;
+    }
+    ShowDatabases showDatabases = new ShowDatabases();
+    showDatabases.showAllDatabases();
+    return true;
+  }
+
+  private boolean exitQuery(List<String> tokens) {
+    if (tokens == null || tokens.size() <= 0 || (";").equals(tokens.get(0))) {
+      PrintInfo.getInstance().printMessage("\n\tExit\n\t");
+      System.exit(0);
     }
 
-    private boolean commit(List<String> tokens){
-        if(tokens == null || tokens.size()<=0 || (";").equals(tokens.get(0))){
-            PrintInfo.getInstance().printMessage("\n\tCommit Successful!\n\t");
-            Commit commit = new Commit();
+    PrintInfo.getInstance().commandError();
+    return false;
+  }
 
-            return true;
-        }
-
-        PrintInfo.getInstance().commandError();
-        return false;
-
+  private boolean commit(List<String> tokens) {
+    if (tokens == null || tokens.size() <= 0 || (";").equals(tokens.get(0))) {
+      PrintInfo.getInstance().printMessage("\n\tCommit Successful!\n\t");
+      Commit commit = new Commit();
+      return true;
     }
 
-    private boolean create(List<String> tokens){
-        if(tokenListValidation(tokens)){
-            switch (tokens.get(0).toLowerCase()){
-                case "database" : {
-                    return (createDatabase(getSubTokens(tokens)));
-                }
-                case "table" : {
-                    return(createTable(getSubTokens(tokens)));
-                }
-                default:{
-                    PrintInfo.getInstance().commandError();
-                    return false;
-                }
-            }
+    PrintInfo.getInstance().commandError();
+    return false;
+  }
+
+  private boolean create(List<String> tokens) {
+    if (tokenListValidation(tokens)) {
+      switch (tokens.get(0).toLowerCase()) {
+        case "database" -> {
+          return (createDatabase(getSubTokens(tokens)));
         }
-        else{
-            PrintInfo.getInstance().commandError();
+        case "table" -> {
+          return (createTable(getSubTokens(tokens)));
         }
-        return false;
+        default -> {
+          PrintInfo.getInstance().commandError();
+          return false;
+        }
+      }
+    } else {
+      PrintInfo.getInstance().commandError();
+    }
+    return false;
+  }
+
+  private boolean createDatabase(List<String> tokens) {
+    if (!tokenListValidation(tokens)) {
+      PrintInfo.getInstance().commandError();
+      return false;
+    }
+    if (!regexValidationOfName(tokens.get(0))) {
+      PrintInfo.getInstance().printError("\n\tDatabase Name should only contain characters\n");
+      return false;
+    }
+    String name = tokens.get(0);
+    tokens = getSubTokens(tokens);
+    if (tokens == null || tokens.size() <= 0 || (";").equals(tokens.get(0))) {
+      CreateDatabase createDatabase = new CreateDatabase();
+      return (createDatabase.addDatabase(name));
+    }
+    PrintInfo.getInstance().commandError();
+    return false;
+  }
+
+  private boolean createTable(List<String> tokens) {
+    if (!tableQueryBasicCheck()) {
+      return false;
+    }
+    if (!tokenListValidation(tokens)) {
+      PrintInfo.getInstance().commandError();
+      return false;
+    }
+    if (!regexValidationOfName(tokens.get(0))) {
+      PrintInfo.getInstance().printError("\n\tTable Name should only contain characters\n");
+      return false;
+    }
+    String name = tokens.get(0);
+    tokens = getSubTokens(tokens);
+    if (tokens == null || tokens.size() <= 0 || (";").equals(tokens.get(0))) {
+      PrintInfo.getInstance().printError("\n\tEnter input columns for the tables\n");
+      return false;
+    }
+    List<String> columns = fetchColumnsForTableCreate(tokens);
+    if (columns == null) {
+      return false;
+    }
+    Map<String, String> columnAndType = fetchColumnAndTypeMap(columns);
+    if (columnAndType == null) {
+      return false;
     }
 
-    private boolean createDatabase(List<String> tokens){
-        if(!tokenListValidation(tokens)){
-            PrintInfo.getInstance().commandError();
-            return false;
-        }
-        if(!regexValidationOfName(tokens.get(0))){
-            PrintInfo.getInstance().printError("\n\tDatabase Name should only contain characters\n");
-            return false;
-        }
-        String name = tokens.get(0);
-        tokens = getSubTokens(tokens);
-        if(tokens == null || tokens.size()<=0 || (";").equals(tokens.get(0))){
-            CreateDatabase createDatabase = new CreateDatabase();
-            return(createDatabase.addDatabase(name));
-        }
-        PrintInfo.getInstance().commandError();
-        return false;
+    CreateTable createTable = new CreateTable();
+    return createTable.addTable(name, columnAndType);
+  }
+
+  private List<String> fetchColumnsForTableCreate(List<String> tokens) {
+    if (!("(").equals(tokens.get(0))) {
+      PrintInfo.getInstance().commandError();
+      return null;
     }
-
-    private boolean createTable(List<String> tokens){
-        if(!tableQueryBasicCheck()){
-            return false;
-        }
-        if(!tokenListValidation(tokens)){
-            PrintInfo.getInstance().commandError();
-            return false;
-        }
-        if(!regexValidationOfName(tokens.get(0))){
-            PrintInfo.getInstance().printError("\n\tTable Name should only contain characters\n");
-            return false;
-        }
-        String name = tokens.get(0);
-        tokens = getSubTokens(tokens);
-        if(tokens == null || tokens.size()<=0 || (";").equals(tokens.get(0))){
-            PrintInfo.getInstance().printError("\n\tEnter input columns for the tables\n");
-            return false;
-        }
-        List<String> columns = fetchColumnsForTableCreate(tokens);
-        if(columns == null){
-            return false;
-        }
-        Map<String, String> columnAndType = fetchColumnAndTypeMap(columns);
-        if(columnAndType == null){
-            return false;
-        }
-
-        CreateTable createTable = new CreateTable();
-        return createTable.addTable(name, columnAndType);
+    tokens = getSubTokens(tokens);
+    if (!tokenListValidation(tokens)) {
+      PrintInfo.getInstance().commandError();
+      return null;
     }
+    int lastIndex = tokens.size() - 1;
+    String last = tokens.get(lastIndex);
+    String secondLast = tokens.get(lastIndex - 1);
+    if (")".equals(last)) {
+      return tokens.subList(0, lastIndex);
+    } else if (";".equals(last) && ")".equals(secondLast)) {
+      return tokens.subList(0, lastIndex - 1);
+    }
+    PrintInfo.getInstance().commandError();
+    return null;
+  }
 
-    private List<String> fetchColumnsForTableCreate(List<String> tokens){
-        if(!("(").equals(tokens.get(0))){
-            PrintInfo.getInstance().commandError();
-            return null;
-        }
-        tokens = getSubTokens(tokens);
-        if(!tokenListValidation(tokens)){
-            PrintInfo.getInstance().commandError();
-            return null;
-        }
-        int lastIndex = tokens.size()-1;
-        String last = tokens.get(lastIndex);
-        String secondLast = tokens.get(lastIndex-1);
-        if(")".equals(last)){
-            return tokens.subList(0, lastIndex);
-        }
-        else if(";".equals(last) && ")".equals(secondLast)){
-            return tokens.subList(0, lastIndex-1);
-        }
-
+  private Map<String, String> fetchColumnAndTypeMap(List<String> tokens) {
+    Map<String, String> columnsWithDataType = new LinkedHashMap<>();
+    while (tokens != null && tokens.size() > 0) {
+      if (tokens.size() < 2) {
         PrintInfo.getInstance().commandError();
         return null;
-    }
-
-    private Map<String, String> fetchColumnAndTypeMap(List<String> tokens){
-        Map<String, String> columnsWithDataType = new LinkedHashMap<>();
-        for(;;){
-            if(tokens == null || tokens.size()<=0){
-                break;
-            }
-            if(tokens.size() < 2){
-                PrintInfo.getInstance().commandError();
-                return null;
-            }
-            String columnName = tokens.get(0).toUpperCase();
-            String columnType = tokens.get(1);
-            if(!regexValidationOfName(columnName) || !regexValidationOfName(columnType) || columnDataTypeValidation(columnType) == null){
-                PrintInfo.getInstance().commandError();
-                return null;
-            }
-
-            columnType = columnDataTypeValidation(columnType);
-
-            columnsWithDataType.put(columnName, columnType);
-            tokens = getSubTokens(getSubTokens(tokens));
-            if(tokens!=null && tokens.size()>0){
-                if((",").equals(tokens.get(0))){
-                    tokens = getSubTokens(tokens);
-                }
-                else{
-                    PrintInfo.getInstance().commandError();
-                    return null;
-                }
-            }
+      }
+      String columnName = tokens.get(0).toUpperCase();
+      String columnType = tokens.get(1);
+      if (!regexValidationOfName(columnName) || !regexValidationOfName(columnType) || columnDataTypeValidation(columnType) == null) {
+        PrintInfo.getInstance().commandError();
+        return null;
+      }
+      columnType = columnDataTypeValidation(columnType);
+      columnsWithDataType.put(columnName, columnType);
+      tokens = getSubTokens(getSubTokens(tokens));
+      if (tokens != null && tokens.size() > 0) {
+        if ((",").equals(tokens.get(0))) {
+          tokens = getSubTokens(tokens);
+        } else {
+          PrintInfo.getInstance().commandError();
+          return null;
         }
-        return columnsWithDataType;
+      }
     }
+    return columnsWithDataType;
+  }
 
-    private boolean regexValidationOfName(String name){
-        String regex = "^[a-zA-Z]++$";
+  private boolean regexValidationOfName(String name) {
+    String regex = "^[a-zA-Z]++$";
 
-        Pattern VALIDATE = Pattern.compile(regex,
-                Pattern.CASE_INSENSITIVE);
-        if (!VALIDATE.matcher(name).matches()) {
-            return false;
-        }
-        return true;
+    Pattern VALIDATE = Pattern.compile(regex,
+        Pattern.CASE_INSENSITIVE);
+    if (!VALIDATE.matcher(name).matches()) {
+      return false;
     }
+    return true;
+  }
 
-    private List<String> getSubTokens(List<String> tokens){
-        List<String> newTokens = null;
-        if(tokens.size()>1){
-            newTokens = tokens.subList(1, tokens.size());
-        }
-        return newTokens;
+  private List<String> getSubTokens(List<String> tokens) {
+    List<String> newTokens = null;
+    if (tokens.size() > 1) {
+      newTokens = tokens.subList(1, tokens.size());
     }
+    return newTokens;
+  }
 
-    private boolean tokenListValidation(List<String> tokens){
-        return(tokens != null && tokens.size()>=1);
+  private boolean tokenListValidation(List<String> tokens) {
+    return (tokens != null && tokens.size() >= 1);
+  }
+
+  private String columnDataTypeValidation(String datatype) {
+    switch (datatype.toLowerCase()) {
+      case "string" -> {
+        return "String";
+      }
+      case "double" -> {
+        return "Double";
+      }
+      case "boolean" -> {
+        return "Boolean";
+      }
+      case "int" -> {
+        return "Integer";
+      }
+      case "Integer" -> {
+        return "Integer";
+      }
+      default -> {
+        return null;
+      }
     }
+  }
 
-    private String columnDataTypeValidation(String datatype){
-        switch(datatype.toLowerCase()){
-            case "string" : {
-                return "String";
-            }
-            case "double" : {
-                return "Double";
-            }
-            case "boolean" : {
-                return "Boolean";
-            }
-            case "int" : {
-                return "Integer";
-            }
-            case "integer" : {
-                return "Integer";
-            }
-            default:{
-                return null;
-            }
-
-        }
+  private boolean tableQueryBasicCheck() {
+    if (BasicInformation.getInstance().fetchDatabase() == null) {
+      PrintInfo.getInstance().printError("\n\tSelect Database First!!!\n");
+      return false;
     }
+    return true;
+  }
 
-    private boolean tableQueryBasicCheck(){
-        if(BasicInformation.getInstance().fetchDatabase() == null){
-            PrintInfo.getInstance().printError("\n\tSelect Database First!!!\n");
-            return false;
-        }
-        return true;
-    }
-
-    private boolean endOfQuery(List<String> tokens){
-        return (tokens == null || tokens.size()<=0 || (";").equals(tokens.get(0)));
-    }
-
+  private boolean endOfQuery(List<String> tokens) {
+    return (tokens == null || tokens.size() <= 0 || (";").equals(tokens.get(0)));
+  }
 }
